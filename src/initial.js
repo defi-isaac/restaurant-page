6ae6dc76a872e8f6787b3b4c7d927960f8d1ff85
Isaac Duong <isaacduong157@gmail.com>
2023-09-03 13:47:42 +1000

    Implement pageLoad function on separate module
    This replaces the old HTML we had before

--- src/initial.js
+++ src/initial.js
@@ -1,20 +1,25 @@
 export function pageLoad() {
     const container = document.getElementById('container');
     const header = document.createElement('header');
-    const logo = document.createElement('div').classList.add('logo');
+    const logo = document.createElement('div');
+    logo.classList.add('logo');
     const navList = document.createElement('ul');
-    const directions = document.createElement('div').classList.add('directions');
-    const content = document.createElement('div').classList.add('content');
-    const imgDisplay = document.createElement('div').classList.add('imgDisplay');
-    const socialMedia = document.createElement('div').classList.add('socialMedia', 'unique');
+    const directions = document.createElement('div');
+    directions.classList.add('directions');
+    const content = document.createElement('div');
+    content.classList.add('content');
+    const imgDisplay = document.createElement('div');
+    imgDisplay.classList.add('imgDisplay');
+    const socialMedia = document.createElement('div')
+    socialMedia.classList.add('socialMedia', 'unique');
     const footer = document.createElement('footer')
 
+    container.appendChild(header);
+    container.appendChild(content);
+    container.appendChild(footer);
     header.appendChild(logo);
     header.appendChild(navList);
     header.appendChild(directions);
     content.appendChild(imgDisplay);
     content.appendChild(socialMedia);
-    container.appendChild(header);
-    container.appendChild(content);
-    container.appendChild(footer);
 }
