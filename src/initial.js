d4cf92bce532e611c51bc705f95549954a9c60a4
Isaac Duong <isaacduong157@gmail.com>
2023-09-03 21:52:04 +1000

    Complete HTML nav bar

--- src/initial.js
+++ src/initial.js
@@ -1,25 +1,38 @@
 export function pageLoad() {
-    const container = document.getElementById('container');
-    const header = document.createElement('header');
-    const logo = document.createElement('div');
-    logo.classList.add('logo');
-    const navList = document.createElement('ul');
-    const directions = document.createElement('div');
-    directions.classList.add('directions');
-    const content = document.createElement('div');
-    content.classList.add('content');
-    const imgDisplay = document.createElement('div');
-    imgDisplay.classList.add('imgDisplay');
-    const socialMedia = document.createElement('div')
-    socialMedia.classList.add('socialMedia', 'unique');
-    const footer = document.createElement('footer')
+    // const container = document.getElementById('container');
+    // const header = document.createElement('header');
+    // const logo = document.createElement('div');
+    // logo.classList.add('logo');
+    // const location = document.createElement('div');
+    // location.classList.add('location')
+    // const navList = document.createElement('ul');
+    // const directions = document.createElement('a');
+    // directions.classList.add('directions');
+    // const content = document.createElement('div');
+    // content.classList.add('content');
+    // const imgDisplay = document.createElement('div');
+    // imgDisplay.classList.add('imgDisplay');
+    // const socialMedia = document.createElement('div')
+    // socialMedia.classList.add('socialMedia', 'unique');
+    // const footer = document.createElement('footer');
+    // const newLi = () => {
+    //     return document.createElement('li');
+    // }
 
-    container.appendChild(header);
-    container.appendChild(content);
-    container.appendChild(footer);
-    header.appendChild(logo);
-    header.appendChild(navList);
-    header.appendChild(directions);
-    content.appendChild(imgDisplay);
-    content.appendChild(socialMedia);
+    // logo.textContent = 'Vague de feu';
+    // location.textContent = "SYDNEY"
+    // directions.textContent = "DIRECTIONS"
+
+    // container.appendChild(header);
+    // container.appendChild(content);
+    // container.appendChild(footer);
+    // header.appendChild(logo);
+    // logo.appendChild(location);
+    // header.appendChild(navList);
+    // navList.appendChild(newLi());
+    // navList.appendChild(newLi());
+    // navList.appendChild(newLi());
+    // header.appendChild(directions);
+    // content.appendChild(imgDisplay);
+    // content.appendChild(socialMedia);
 }
