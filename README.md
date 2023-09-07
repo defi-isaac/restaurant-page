8dd089cc1f4d97bf5667bd0d3c19a291dd6718df
Isaac Duong <isaacduong157@gmail.com>
2023-09-07 13:14:46 +1000

    Initial dist subtree commit

--- README.md
+++
@@ -1,6 +0,0 @@
-# restaurant-page
-
-Recreating Nobu Sydney's website. Website not used for commercial purposes.
-
-INSPIRATION: https://noburestaurants.com/sydney/nobu-sidney-japanese-restaurant-bar-and-lounge/
-NOBU WEBSITE DONE BY: https://www.pastpresentfuture.com/
--- reservation.js
+++
@@ -1,8 +0,0 @@
-import {imgDisplay, subContent} from './layout.js';
-
-export function reservationPage() {
-    imgDisplay.style.backgroundImage = 'url("../src/images/food.avif")';
-    imgDisplay.className = 'imgDisplay reservation';
-    
-    subContent.className = 'unique socialMedia';
-}
